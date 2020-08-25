45d94fb5fe1adec58023c47468d6f3b7ed6b59f4
devWitch <tiffany.valentina87@gmail.com>
2020-08-25 15:50:43 -0600

    First Commit

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+export const clientId = "64a82e57d6724c79bf6e4cbf119f68a0";
+export const redirectUri = "http://localhost:3000/";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
